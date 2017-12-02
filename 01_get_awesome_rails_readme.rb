0714fd3fd5cc2fa0aa7b22af66dc2e1e56efcd3d
kikunantoka <kikunantoka@gmail.com>
2017-12-02 11:53:25 +0900

    Fix folder formations

--- 01_get_awesome_rails_readme.rb
+++
@@ -1,2 +0,0 @@
-require 'open3'
-Open3.capture3('wget -O 11_awesome_rails_readme.md https://raw.githubusercontent.com/ekremkaraca/awesome-rails/master/README.md')
---
+++ scripts/01_get_awesome_rails_readme.rb
@@ -0,0 +1,2 @@
+require 'open3'
+Open3.capture3('wget -O files/01_awesome_rails_readme.md https://raw.githubusercontent.com/ekremkaraca/awesome-rails/master/README.md')
