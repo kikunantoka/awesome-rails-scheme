7527bc2198ccdf9bfb3a80c4b11f38cdd7ac19f1
kikunantoka <kikunantoka@gmail.com>
2017-12-02 14:29:22 +0900

    Add creating repository data

--- scripts/02_get_repository_data.rb
+++ scripts/02_get_repository_data.rb
@@ -1,3 +1,10 @@
+require "active_record"
+require "yaml"
+require "./app/models/repository"
+
+config = YAML.load_file('./config/database.yml')
+ActiveRecord::Base.establish_connection(config["development"])
+
 is_app_list = false
 
 begin
@@ -5,7 +12,7 @@ begin
     file.each_line do |line|
       is_app_list = true if line.include?("### Apps Made with Rails")
       is_app_list = false if line.include?("### Tutorials")
-      puts line.match(%r{https://github.com/(.+?)\)})[1] if is_app_list && line.include?("https://github.com")
+      Repository.find_or_create_by!(name: line.match(%r{https://github.com/(.+?)\)})[1]) if is_app_list && line.include?("https://github.com")
     end
   end
 rescue SystemCallError => e
